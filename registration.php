d11cdc0f8aa599d372702b6873be4da1c15c6d94
Shakib117 <md.shakib2k@gmail.com>
2024-01-18 20:41:04 +0600

    Authentication added

---
+++ registration.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <!-- fontawesome linlup -->
+    <script src="https://kit.fontawesome.com/83cc78d2bd.js" crossorigin="anonymous"></script>
+    <!-- bootstrap linkup -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- css linkup -->
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+  <section class="mt-5 ">
+    <div class="container">
+      <div class="row">
+        <h3 class="heading">WELCOME TO (SITE NAME)</h3>
+      
+        <form class="row g-3">
+          <div class="col-md-12">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email">
+          </div>
+
+          <div class="d-flex">
+            <div class="col-md-6 me-2">
+              <label for="fname" class="form-label">First Name</label>
+              <input type="text" class="form-control" id="fname">
+            </div>
+
+            <div class="col-md-6">
+              <label for="lname" class="form-label">Last Name</label>
+              <input type="text" class="form-control" id="lname">
+            </div>
+          </div>
+
+          <div class="col-12">
+            <label for="address" class="form-label">Address</label>
+            <input type="text" class="form-control" id="address">
+          </div>
+
+          <div class="d-flex">
+            <div class="col-md-6 me-2">
+              <label for="country" class="form-label">Country</label>
+              <input type="text" class="form-control" id="country">
+            </div>
+
+            <div class="col-md-6">
+              <label for="number" class="form-label">Phone Number</label>
+              <input type="text" class="form-control" id="number">
+            </div>
+          </div>
+          <div class="d-flex">
+            <div class="col-md-6 me-2">
+              <label for="pass" class="form-label">Password</label>
+              <input type="password" class="form-control" id="pass">
+            </div>
+
+            <div class="col-md-6">
+              <label for="conpass" class="form-label">Confirm Password</label>
+              <input type="password" class="form-control" id="conpass">
+            </div>
+          </div>
+
+          <div class="d-flex justify-content-center">
+            <button type="submit" class="sign-up-btn">SIGN UP</button>
+          </div>   
+        </form>
+      </div>
+    </div>
+    
+    <div class="d-flex justify-content-center mt-3">
+      <h5>Already have an account? <a href="signin.php">SIGN IN</a></h5>
+    </div>  
+  </section>
+
+
+  <!-- bootstrap linkup -->
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+
+</body>
+</html>
--- signin.php
+++ signin.php
@@ -1,27 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Business Project</title>
+    <title>Sign In</title>
+     <!-- fontawesome linlup -->
+    <script src="https://kit.fontawesome.com/83cc78d2bd.js" crossorigin="anonymous"></script>
     <!-- bootstrap linkup -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <!-- fontawesome linlup -->
-    <script src="https://kit.fontawesome.com/83cc78d2bd.js" crossorigin="anonymous"></script>
-    <!-- css linkup -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- css linkup -->
     <link rel="stylesheet" href="css/style.css">
 </head>
-
 <body>
-    
+    <section class="mt-5">
+      <div class="container">
+        <div class="row">
+            <div class="col col-lg-6 d-flex justify-content-center align-items-center" >
+              <h1>PROJECT LOGO OR NAME</h1>
+            </div>
+            <div class="col col-lg-6 login-form">
+                <form action="">
+                    <div class="d-flex justify-content-center">
+                      <i class="fa-regular fa-circle-user"></i>
+                    </div>
+                    <input type="text" placeholder="   Email" class="rounded mt-3 py-2">
+                    
+                    <input type="password" placeholder="   Password" class="rounded mt-3 py-2">
+                    
+                    <div class="d-flex justify-content-center mt-3">
+                      <button class="sign-in-btn">SIGN IN</button>
+                    </div>
 
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+                    <div class="d-flex justify-content-center mt-3">
+                      <a href="">FORGOTTEN PASSWORD?</a>
+                     </div>
+                </form>
+
+                <h5 class="mt-3 d-flex justify-content-center">Don't you have any account?</h5>
+
+                <div class="mt-3 d-flex justify-content-center">
+                  <a href="registration.php"><button class="create-account-btn">CREATE AN ACCOUNT</button></a>
+                </div>
+            </div>
+          </div>
+      </div>
+    </section>
+
+
+      <!-- bootstrap linkup -->
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"></script>
 </body>
-
 </html>
