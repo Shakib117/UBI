279cadb10908d3013d68319b94b27421b5d0547c
Shakib117 <md.shakib2k@gmail.com>
2024-03-20 22:26:51 +0600

    admin related

--- admin/inc/sidebar.php
+++ admin/inc/sidebar.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- bootstrap linkup -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
     <!-- fontawesome linlup -->
-    <script src="https://kit.fontawesome.com/83cc78d2bd.js" crossorigin="anonymous"></script>
+    <!-- <script src="https://kit.fontawesome.com/83cc78d2bd.js" crossorigin="anonymous"></script> -->
     <!-- css linkup -->
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <!-- <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css"> -->
     <title>Document</title>
 </head>
 
@@ -19,7 +19,7 @@
         <h3>sidebar</h3>
         <ul class="pt-5">
             <li>
-                <button class="btn" data-bs-toggle="collapse" data-bs-target="#once">1</button>
+                <button class="btn" data-bs-toggle="collapse" data-bs-target="#once" aria-expanded="false">1</button>
                 <ul id="once" class="collapse bg-light">
                     <li class="text-bg-info">1.1</li>
                     <li class="text-bg-info">1.2</li>
@@ -28,7 +28,7 @@
                 </ul>
             </li>
             <li>
-                <button class="btn" data-bs-toggle="collapse" data-bs-target="#twice">2</button>
+                <button class="btn" data-bs-toggle="collapse" data-bs-target="#twice" aria-expanded="false">2</button>
                 <ul id="twice" class="collapse bg-light">
                     <li class="text-bg-info">2.1</li>
                     <li class="text-bg-info">2.2</li>
@@ -41,9 +41,10 @@
             <li>5</li>
         </ul>
     </div>
+
     <!-- bootstrap linkup -->
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    <!-- <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"> -->
     </script>
 </body>
 
--- admin/index.php
+++ admin/index.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- bootstrap linkup -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- fontawesome linlup -->
     <script src="https://kit.fontawesome.com/83cc78d2bd.js" crossorigin="anonymous"></script>
     <!-- css linkup -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
