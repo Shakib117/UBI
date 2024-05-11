bd370b9f633d072adb8d0342cb369e5962279ea1
Shakib117 <md.shakib2k@gmail.com>
2024-05-11 19:37:15 +0600

    Minor changes

--- admin/index.php
+++ admin/index.php
@@ -15,32 +15,8 @@
 </head>
 
 <body>
-    <div class="sidebar">
-        <h3>sidebar</h3>
-        <ul class="pt-5">
-            <li>
-                <button class="btn" data-bs-toggle="collapse" data-bs-target="#once">1</button>
-                <ul id="once" class="collapse bg-light">
-                    <li class="text-bg-info">1.1</li>
-                    <li class="text-bg-info">1.2</li>
-                    <li class="text-bg-info">1.3</li>
-                    <li class="text-bg-info">1.4</li>
-                </ul>
-            </li>
-            <li>
-                <button class="btn" data-bs-toggle="collapse" data-bs-target="#twice">2</button>
-                <ul id="twice" class="collapse bg-light">
-                    <li class="text-bg-info">2.1</li>
-                    <li class="text-bg-info">2.2</li>
-                    <li class="text-bg-info">2.3</li>
-                    <li class="text-bg-info">2.4</li>
-                </ul>
-            </li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-        </ul>
-    </div>
+    <?php include "inc/sidebar.php";?>
+    
     <!-- bootstrap linkup -->
     <script src="js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
